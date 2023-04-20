b0cc88567ec231006fe9beed169921b4900b43a8
Guanyun <guanyun1024@icloud.com>
2023-04-20 10:52:15 +0800

    filtering the product list

--- 99-practice/SportsSln/SportsStore/Controllers/HomeController.cs
+++ 99-practice/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -13,13 +13,18 @@ public class HomeController : Controller {
         repository = repo;
     }
 
-    public ViewResult Index(int productPage = 1)
+    public ViewResult Index(string? category, int productPage = 1)
         => View(new ProductsListViewModel {
-            Products = repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
+            Products = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
             PagingInfo = new PagingInfo {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
                 TotalItems = repository.Products.Count(),
-            }
+            },
+            CurrentCategory = category,
         });
 }
