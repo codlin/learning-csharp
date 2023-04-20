cba4e5cbcb25fab60f9a284a997389a477d1f367
Guanyun <guanyun1024@icloud.com>
2023-04-20 10:30:00 +0800

    styling the content

--- 99-practice/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ 99-practice/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -21,6 +21,12 @@ public class PageLinkTagHelper : TagHelper {
     public ViewContext? ViewContext { get; set; }
     public PagingInfo? PageModel { get; set; }
     public string? PageAction { get; set; }
+
+    public bool PageClassesEnabled { get; set; } = false;
+    public string PageClass { get; set; } = String.Empty;
+    public string PageClassNormal { get; set; } = String.Empty;
+    public string PageClassSelected { get; set; } = String.Empty;
+
     public override void Process(TagHelperContext context, TagHelperOutput output) {
         if (ViewContext != null && PageModel != null) {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -28,6 +34,11 @@ public class PageLinkTagHelper : TagHelper {
             for (int i = 1; i <= PageModel.TotalPages; i++) {
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                if (PageClassesEnabled) {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
