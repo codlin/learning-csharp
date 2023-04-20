831d298d9decb47c5cfad200737d8134845855bf
Guanyun <guanyun1024@icloud.com>
2023-04-20 08:22:41 +0800

    update

--- 99-practice/SportsSln/SportsStore.Tests/HomeControllerTests.cs
+++ 99-practice/SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -6,11 +6,9 @@ using SportsStore.Models;
 
 namespace SportsStore.Tests;
 
-public class HomeControllerTests
-{
+public class HomeControllerTests {
     [Fact]
-    public void Can_Use_Repository()
-    {
+    public void Can_Use_Repository() {
         // Arrange
         Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
         mock.Setup(m => m.Products).Returns((
@@ -28,5 +26,30 @@ public class HomeControllerTests
         Assert.True(prodArray.Length == 2);
         Assert.Equal("P1", prodArray[0].Name);
         Assert.Equal("P2", prodArray[1].Name);
+    }
+
+    [Fact]
+    public void Can_Paginate() {
+        // Arrange
+        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+        mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"},
+            new Product {ProductID = 3, Name = "P3"},
+            new Product {ProductID = 4, Name = "P4"},
+            new Product {ProductID = 5, Name = "P5"}
+        }).AsQueryable<Product>());
+        HomeController controller = new HomeController(mock.Object);
+        controller.PageSize = 3;
+
+        // Act
+        IEnumerable<Product> result = (controller.Index(2) as ViewResult)?.ViewData.Model
+            as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+
+        // Assert
+        Product[] prodArray = result.ToArray();
+        Assert.True(prodArray.Length == 2);
+        Assert.Equal("P4", prodArray[0].Name);
+        Assert.Equal("P5", prodArray[1].Name);
     }
 }
