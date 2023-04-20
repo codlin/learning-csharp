a2400461823fee67876a2017d6f38ae2a639294d
Guanyun <guanyun1024@icloud.com>
2023-04-20 10:00:26 +0800

    add pagination

--- 99-practice/SportsSln/SportsStore/Program.cs
+++ 99-practice/SportsSln/SportsStore/Program.cs
@@ -3,14 +3,14 @@ using SportsStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<StoreDbContext>(opts =>
-{
+builder.Services.AddDbContext<StoreDbContext>(opts => {
     opts.UseNpgsql(@$"Host={Environment.GetEnvironmentVariable("POSTGRES_HOST")};Username=test;Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWD")};Database=SportsStore");
 });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 var app = builder.Build();
 app.UseStaticFiles();
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index" });
 app.MapDefaultControllerRoute();
 
 SeedData.EnsurePopulated(app);
