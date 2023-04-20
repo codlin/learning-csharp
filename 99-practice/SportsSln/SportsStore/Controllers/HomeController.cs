55703829a7f74411ca11d9d4f7a5884b90590d14
Guanyun <guanyun1024@icloud.com>
2023-04-20 14:22:48 +0800

    add navigation controld

--- 99-practice/SportsSln/SportsStore/Controllers/HomeController.cs
+++ 99-practice/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -23,7 +23,9 @@ public class HomeController : Controller {
             PagingInfo = new PagingInfo {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count(),
+                TotalItems = category == null
+                    ? repository.Products.Count()
+                    : repository.Products.Where(e => e.Category == category).Count()
             },
             CurrentCategory = category,
         });
