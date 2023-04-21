8268cb7e07e353157d65877db34c2a5fcc8a2738
Guanyun <guanyun1024@icloud.com>
2023-04-21 10:22:59 +0800

    refining the cart model with a service

--- 99-practice/SportsSln/SportsStore/Models/Cart.cs
+++ 99-practice/SportsSln/SportsStore/Models/Cart.cs
@@ -2,7 +2,7 @@ namespace SportsStore.Models;
 
 public class Cart {
     public List<CartLine> Lines { get; set; } = new();
-    public void AddItem(Product product, int quantity) {
+    public virtual void AddItem(Product product, int quantity) {
         CartLine? line = Lines.Where(p => p.Product.ProductID == product.ProductID).FirstOrDefault();
         if (line == null) {
             Lines.Add(new CartLine {
@@ -14,7 +14,7 @@ public class Cart {
         }
     }
 
-    public void RemoveLine(Product product) {
+    public virtual void RemoveLine(Product product) {
         Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
     }
 
@@ -22,7 +22,7 @@ public class Cart {
         return Lines.Sum(e => e.Product.Price * e.Quantity);
     }
 
-    public void Clear() => Lines.Clear();
+    public virtual void Clear() => Lines.Clear();
 }
 
 public class CartLine {
--- 99-practice/SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ 99-practice/SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -8,24 +8,25 @@ namespace SportsStore.Pages;
 public class CartModel : PageModel {
     private IStoreRepository repository;
 
-    public CartModel(IStoreRepository repo) {
+    public CartModel(IStoreRepository repo, Cart cartService) {
         repository = repo;
+        Cart = cartService;
     }
 
-    public Cart? Cart { get; set; }
+    public Cart Cart { get; set; }
     public string ReturnUrl { get; set; } = "/";
 
     public void OnGet(string returnUrl) {
         ReturnUrl = returnUrl ?? "/";
-        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new();
+        // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new();
     }
 
     public IActionResult OnPost(long productID, string returnUrl) {
         Product? product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
         if (product != null) {
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new();
+            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
+            // HttpContext.Session.SetJson("cart", Cart);
         }
         return RedirectToPage(new { returnUrl = returnUrl });
     }
