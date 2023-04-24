ff92b781e1c284eb4fe1ea90b81ac79cb4a8a473
Guanyun <guanyun1024@icloud.com>
2023-04-24 16:51:29 +0800

    Constraining Matching to a Specific Set of Values

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -5,14 +5,16 @@ var app = builder.Build();
 
 // app.MapGet("files/{filename}.{ext}", async context => {
 // app.MapGet("{first}/{second}/{*catchall}", async context => {
-app.MapGet("{first:int}/{second:bool}", async context => {
+// app.MapGet("{first:int}/{second:bool}", async context => {
+app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
     await context.Response.WriteAsync("Request Was Routed\n");
     foreach (var kvp in context.Request.RouteValues) {
         await context.Response
         .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
     }
 });
-app.MapGet("capital/{country=France}", Capital.Endpoint);
+// app.MapGet("capital/{country=France}", Capital.Endpoint);
+app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
 
 app.Run();
