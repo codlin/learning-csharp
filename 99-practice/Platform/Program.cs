df7837839186c25c3102746af55216ad8020e050
Guanyun <guanyun1024@icloud.com>
2023-04-24 11:42:06 +0800

    Creating Pipeline Branches

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
 app.Use(async (context, next) => {
     await next();
     await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
@@ -11,6 +12,13 @@ app.Use(async (context, next) => {
     } else {
         await next();
     }
+});
+
+((IApplicationBuilder)app).Map("/branch", branch => {
+    branch.UseMiddleware<Platform.QueryStringMiddleware>();
+    branch.Use(async (HttpContext context, Func<Task> next) => {
+        await context.Response.WriteAsync($"Branch Middleware");
+    });
 });
 
 app.UseMiddleware<Platform.QueryStringMiddleware>();
