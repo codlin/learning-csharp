1b47cc15e9d128157e95c6110f0a170cc7e950c1
Guanyun <guanyun1024@icloud.com>
2023-04-24 11:31:27 +0800

    Short-Circuiting the Request Pipeline

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -2,17 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Use(async (context, next) => {
     await next();
-    await context.Response
-    .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+    await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
 });
 
 app.Use(async (context, next) => {
-    if (context.Request.Method == HttpMethods.Get
-            && context.Request.Query["custom"] == "true") {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom Middleware \n");
+    if (context.Request.Path == "/short") {
+        await context.Response.WriteAsync($"Request Short Circuited");
+    } else {
+        await next();
     }
-    await next();
 });
 
 app.UseMiddleware<Platform.QueryStringMiddleware>();
