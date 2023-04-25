36207a1f9794a3993016371265007b17d62e1a9a
Guanyun <guanyun1024@icloud.com>
2023-04-25 15:47:47 +0800

    Using an Adapter Function

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -11,7 +11,8 @@ app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter
     await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
 });
 
-app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
+// app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
+app.MapWeather("endpoint/class");
 
 app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) => {
     await formatter.Format(context, "Endpoint Function: It is sunny in LA");
--- 99-practice/Platform/WeatherEndpoint.cs
+++ 99-practice/Platform/WeatherEndpoint.cs
@@ -3,8 +3,11 @@ using Platform.Services;
 namespace Platform;
 
 public class WeatherEndpoint {
-    public static async Task Endpoint(HttpContext context) {
-        IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+    // public static async Task Endpoint(HttpContext context) {
+    //     IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+    //     await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
+    // }
+    public static async Task Endpoint(HttpContext context, IResponseFormatter formatter) {
         await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
     }
 }
