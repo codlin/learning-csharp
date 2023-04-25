01f18b38cc93eba5ca7e8c23a191a09a6c3ef3ed
Guanyun <guanyun1024@icloud.com>
2023-04-25 16:49:05 +0800

    Avoiding the Transient Service Reuse Pitfall

--- 99-practice/Platform/WeatherMiddleware.cs
+++ 99-practice/Platform/WeatherMiddleware.cs
@@ -9,6 +9,7 @@ public class WeatherMiddleware {
         // formatter = respFormatter;
     }
     public async Task Invoke(HttpContext context, IResponseFormatter formatter) {
+        await formatter.Format(context, "Middleware Class: It will be raining in London\n");
         if (context.Request.Path == "/middleware/class") {
             await formatter.Format(context, "Middleware Class: It is raining in London");
         } else {
