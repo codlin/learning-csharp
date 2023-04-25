75611084835c4e60846ff0eec68a89f23097650e
Guanyun <guanyun1024@icloud.com>
2023-04-25 15:33:21 +0800

    Getting Services from the HttpContext Object

--- 99-practice/Platform/WeatherEndpoint.cs
+++ 99-practice/Platform/WeatherEndpoint.cs
@@ -1,7 +1,10 @@
+using Platform.Services;
+
 namespace Platform;
 
 public class WeatherEndpoint {
     public static async Task Endpoint(HttpContext context) {
-        await context.Response.WriteAsync("Endpoint Class: It is cloudy in Milan");
+        IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+        await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
     }
 }
