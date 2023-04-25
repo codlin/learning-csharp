4b4ccb0dc19e004d6e8f2875a31d736281cb0b31
Guanyun <guanyun1024@icloud.com>
2023-04-25 15:23:53 +0800

    Using a Service in a Middleware Class

--- 99-practice/Platform/WeatherMiddleware.cs
+++ 99-practice/Platform/WeatherMiddleware.cs
@@ -1,13 +1,16 @@
+using Platform.Services;
 namespace Platform;
 
 public class WeatherMiddleware {
     private RequestDelegate next;
-    public WeatherMiddleware(RequestDelegate nextDelegate) {
+    private IResponseFormatter formatter;
+    public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter) {
         next = nextDelegate;
+        formatter = respFormatter;
     }
     public async Task Invoke(HttpContext context) {
         if (context.Request.Path == "/middleware/class") {
-            await context.Response.WriteAsync("Middleware Class: It is raining in London");
+            await formatter.Format(context, "Middleware Class: It is raining in London");
         } else {
             await next(context);
         }
