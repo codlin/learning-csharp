9cd40ea1303aa60d6745437882b80e826739bd95
Guanyun <guanyun1024@icloud.com>
2023-04-26 15:52:06 +0800

    Enriching Status Code Responses

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/error.html");
     app.UseStaticFiles();
 }
+
+app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
+app.Use(async (context, next) => {
+    if (context.Request.Path == "/error") {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        await Task.CompletedTask;
+    } else {
+        await next();
+    }
+});
+
 app.Run(context => {
     throw new Exception("Something has gone wrong");
 });
