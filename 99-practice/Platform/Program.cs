64f55c51551fc834b8b7e50f8893c648ef17714a
Guanyun <guanyun1024@icloud.com>
2023-04-27 10:20:12 +0800

    Using Entity Framework Core

--- 99-practice/Platform/Program.cs
+++ 99-practice/Platform/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Platform.Models;
+using Platform.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDistributedMemoryCache(opts => {
     opts.SizeLimit = 200;
 });
+builder.Services.AddResponseCaching();
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+builder.Services.AddDbContext<CalculationContext>(opts => {
+    opts.UseNpgsql(@$"Host={Environment.GetEnvironmentVariable("POSTGRES_HOST")};Username=test;Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWD")};Database=CalcDb");
+});
+
+builder.Services.AddTransient<SeedData>();
 
 var app = builder.Build();
 
@@ -11,4 +22,11 @@ app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
 });
 
-app.Run();
+bool cmdLineInit = (app.Configuration["INITDB"] ?? "false") == "true";
+if (app.Environment.IsDevelopment() || cmdLineInit) {
+    var seedData = app.Services.GetRequiredService<SeedData>();
+    seedData.SeedDatabase();
+}
+if (!cmdLineInit) {
+    app.Run();
+}
--- 99-practice/Platform/SumEndpoint.cs
+++ 99-practice/Platform/SumEndpoint.cs
@@ -1,23 +1,20 @@
-using Microsoft.Extensions.Caching.Distributed;
+using Platform.Models;
 
 namespace Platform;
 
 public class SumEndpoint {
-    public async Task Endpoint(HttpContext context, IDistributedCache cache) {
+    public async Task Endpoint(HttpContext context, CalculationContext dataContext) {
         int count;
         int.TryParse((string?)context.Request.RouteValues["count"], out count);
-        string cacheKey = $"sum_{count}";
-        string totalString = await cache.GetStringAsync(cacheKey);
-        if (totalString == null) {
-            long total = 0;
+        long total = dataContext.Calculations?.FirstOrDefault(c => c.Count == count)?.Result ?? 0;
+        if (total == 0) {
             for (int i = 1; i <= count; i++) {
                 total += i;
             }
-            totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
-            await cache.SetStringAsync(cacheKey, totalString, new DistributedCacheEntryOptions {
-                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(2)
-            });
+            dataContext.Calculations?.Add(new() { Count = count, Result = total });
+            await dataContext.SaveChangesAsync();
         }
+        string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
         await context.Response.WriteAsync(
             $"({DateTime.Now.ToLongTimeString()}) Total for {count}"
             + $" values:\n{totalString}\n");
