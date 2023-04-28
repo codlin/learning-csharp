6c70df91aff24987660c78654006c51b03b8b029
Guanyun <guanyun1024@icloud.com>
2023-04-28 10:09:05 +0800

    Specifying an Action Result Format

--- 99-practice/WebApp/Controllers/ContentController.cs
+++ 99-practice/WebApp/Controllers/ContentController.cs
@@ -16,6 +16,7 @@ public class ContentController : ControllerBase
     public string GetString() => "This is a string response";
 
     [HttpGet("object")]
+    [Produces("application/json")]
     public async Task<ProductBindingTarget> GetObject()
     {
         Product p = await context.Products.FirstAsync();
