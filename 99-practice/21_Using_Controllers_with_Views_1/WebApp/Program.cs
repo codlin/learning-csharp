86962406b6523b95b03d4fedfc57c3d52b7fb5dd
Guanyun <guanyun1024@icloud.com>
2023-05-04 09:45:18 +0800

    Preparing for Chapter 25

--- 99-practice/21_Using_Controllers_with_Views_1/WebApp/Program.cs
+++ 99-practice/21_Using_Controllers_with_Views_1/WebApp/Program.cs
@@ -2,37 +2,23 @@ using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddDbContext<DataContext>(opts =>
 {
     opts.UseNpgsql(@$"Host={Environment.GetEnvironmentVariable("POSTGRES_HOST")};Username=test;Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWD")};Database=Products");
     opts.EnableSensitiveDataLogging(true);
 });
 
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.IsEssential = true;
-});
-
 builder.Services.AddSingleton<CitiesData>();
 
 var app = builder.Build();
-
 app.UseStaticFiles();
-app.UseSession();
-
 app.MapControllers();
-// app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
-var context = app.Services.CreateScope().ServiceProvider
-    .GetRequiredService<DataContext>();
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
 
 app.Run();
