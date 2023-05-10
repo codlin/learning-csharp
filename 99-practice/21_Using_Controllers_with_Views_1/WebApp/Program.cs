caf631e35d644e1f6e589b04c3f05136ca8702d6
Guanyun <guanyun1024@icloud.com>
2023-05-10 16:19:48 +0800

    Preparing for Chapter 31

--- 99-practice/21_Using_Controllers_with_Views_1/WebApp/Program.cs
+++ 99-practice/21_Using_Controllers_with_Views_1/WebApp/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using WebApp.Filters;
@@ -14,15 +14,16 @@ builder.Services.AddDbContext<DataContext>(opts =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<GuidResponseAttribute>();
-builder.Services.Configure<MvcOptions>(opts =>
-{
-    opts.Filters.Add<HttpsOnlyAttribute>();
-    opts.Filters.Add(new MessageAttribute("This is the globally-scoped filter"));
-});
+// builder.Services.Configure<MvcOptions>(opts =>
+// {
+//     opts.Filters.Add<HttpsOnlyAttribute>();
+//     opts.Filters.Add(new MessageAttribute("This is the globally-scoped filter"));
+// });
 
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+app.MapControllerRoute("forms", "controllers/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
